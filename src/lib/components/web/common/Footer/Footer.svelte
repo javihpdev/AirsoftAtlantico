3f6c781088f5f250984696be911354281e8239cc
javihpdev <javierhpdev@gmail.com>
2025-11-22 21:35:58 +0000

    Corregidos los p y los a porque pueden dar problemas

--- src/lib/components/web/common/Footer/Footer.svelte
+++ src/lib/components/web/common/Footer/Footer.svelte
@@ -1,61 +1,61 @@
 <footer
-	class="flex flex-col md:flex-row gap-2 bg-gray-900 p-6 justify-center gap-10 items-center text-center text-white"
+    class="flex flex-col md:flex-row gap-2 bg-gray-900 p-6 justify-center gap-10 items-center text-center text-white"
 >
-<div class="flex flex-col gap-2 text-lg ">
-<p class="text-4xl">SOPORTE</p>
+    <div class="flex flex-col gap-2 text-lg">
+        <p class="text-4xl">SOPORTE</p>
 
-	<a
-		href="/PrivacyPolicy"
-		class="text-gray-400 hover:text-white text-xl underline"
-		>Política de privacidad</a
-	>
+        <a
+            href="/PrivacyPolicy"
+            class="text-gray-400 hover:text-white text-xl underline"
+        >
+            Política de privacidad
+        </a>
 
-	<a href="/TermsOfUse">
-		<p class="mx-2 text-gray-400 hover:text-white underline"
-			>Aviso legal</p
-		>
-	</a>
+        <a
+            href="/TermsOfUse"
+            class="text-gray-400 hover:text-white text-xl underline"
+        >
+            Aviso legal
+        </a>
 
-	<a href="/CookiePolicy">	
-		<p class="text-gray-400 hover:text-white underline"
-			>Política de cookies</p
-		>
-	</a>
-</div>
+        <a
+            href="/CookiePolicy"
+            class="text-gray-400 hover:text-white text-xl underline"
+        >
+            Política de cookies
+        </a>
+    </div>
 
-<span class="bg-gray-200 md:w-0.5 md:h-50 w-full h-0.5 self-stretch"></span>
+    <span class="bg-gray-200 md:w-0.5 md:h-50 w-full h-0.5 self-stretch"></span>
 
-<div class="flex flex-col items-center justify-center gap-2">
-	
-	<p>Copyright © 2025 Asociación de Airsoft del Atlántico. Todos los derechos reservados.</p>
-	<p>Nuestras Redes Sociales:</p>
-	<div class="flex gap-4">
-		<a href="https://www.instagram.com/asoairatlan/" target="_blank" rel="noopener noreferrer">
-			<img
-				src="/images/logoInstragram.png"
-				class="w-10 object-contain transition-all duration-700 hover:scale-110"
-				alt="logo instagram"
-			/>
-		</a>
-		<a href="https://wa.me/34674957386" target="_blank" rel="noopener noreferrer">
-			<img
-				src="/images/logoWhatsapp.png"
-				class="w-10 object-contain transition-all duration-700 hover:scale-110"
-				alt="logo whatsapp"
-			/>
-		</a>
-	</div>
-	<p class="mt-2 text-xs text-gray-400">
-		Web desarrollada por <a
-			href="https://www.javierhpdev.com/"
-			target="_blank"
-			rel="noopener noreferrer"
-			class="underline mt-2 text-xs text-gray-400 hover:text-white">javihpdev</a
-		>
-	</p>
-</div>
-
-
-
-
+    <div class="flex flex-col items-center justify-center gap-2">
+        <p>Copyright © 2025 Asociación de Airsoft del Atlántico. Todos los derechos reservados.</p>
+        <p>Nuestras Redes Sociales:</p>
+        <div class="flex gap-4">
+            <a href="https://www.instagram.com/asoairatlan/" target="_blank" rel="noopener noreferrer">
+                <img
+                    src="/images/logoInstragram.png"
+                    class="w-10 object-contain transition-all duration-700 hover:scale-110"
+                    alt="logo instagram"
+                />
+            </a>
+            <a href="https://wa.me/34674957386" target="_blank" rel="noopener noreferrer">
+                <img
+                    src="/images/logoWhatsapp.png"
+                    class="w-10 object-contain transition-all duration-700 hover:scale-110"
+                    alt="logo whatsapp"
+                />
+            </a>
+        </div>
+        <p class="mt-2 text-xs text-gray-400">
+            Web desarrollada por <a
+                href="https://www.javierhpdev.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="underline hover:text-white"
+            >
+                javihpdev
+            </a>
+        </p>
+    </div>
 </footer>
